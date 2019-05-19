9a304aaff33964cf355f0155f96047cbbffd1a3a
blappm <blappm@student.ethz.ch>
2019-05-19 10:31:21 +0200

    now saves model with highest accuracy (highest validation accurary during training, highest accuracy during submission generation), and loads that model for predictions

--- seggy_unet/main_cil.py
+++ seggy_unet/main_cil.py
@@ -9,7 +9,7 @@ tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0,
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 submission_flag = False
-nr_of_epochs = 100
+nr_of_epochs = 10
 
 if not submission_flag:
     train_path = "data/roadseg/train"
@@ -27,7 +27,7 @@ else:
     count=94
 
 
-data_gen_args = dict(rotation_range=10,
+data_gen_args = dict(rotation_range=45,
                     width_shift_range=0.05,
                     height_shift_range=0.05,
                     shear_range=0.05,
@@ -41,16 +41,20 @@ if(not submission_flag):
     validGen = trainGenerator(2,valid_path,'image', 'label',data_gen_args,save_to_dir = None)
 
 model = unet()
-model_checkpoint = ModelCheckpoint('unet_roadseg.hdf5', monitor='loss',verbose=1, save_best_only=True)
+model_checkpoint_train = ModelCheckpoint('unet_roadseg.hdf5', monitor='val_acc',verbose=1, save_best_only=True)
+model_checkpoint_submit = ModelCheckpoint('unet_roadseg.hdf5', monitor='acc',verbose=1, save_best_only=True)
+
 
 if(not submission_flag):
-    model.fit_generator(trainGen, steps_per_epoch=90, epochs=nr_of_epochs, callbacks=[model_checkpoint, tensorboard],
+    model.fit_generator(trainGen, steps_per_epoch=90, epochs=nr_of_epochs, callbacks=[model_checkpoint_train, tensorboard],
                         validation_data=validGen, validation_steps=10)
 else:
-    model.fit_generator(trainGen, steps_per_epoch=100, epochs=nr_of_epochs, callbacks=[model_checkpoint])
+    model.fit_generator(trainGen, steps_per_epoch=100, epochs=nr_of_epochs, callbacks=[model_checkpoint_submit, tensorboard])
 
 filenames = os.listdir(predict_path)
 testGene = testGenerator(predict_path)
+# load best model from training and predict results
+model.load_weights("./unet_roadseg.hdf5")
 results = model.predict_generator(testGene,count,verbose=1)
 post_results = np.where(results > 0.5, 1, 0)
 
