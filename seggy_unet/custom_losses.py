67dae1378b6668272451f82c1af5c0f7efab6697
blappm <martin.blapp@gmail.com>
2019-06-27 10:25:43 +0200

    custom loss function fixed and working

--- seggy_unet/custom_losses.py
+++ seggy_unet/custom_losses.py
@@ -15,8 +15,8 @@ def kaggle_metric(y_true, y_pred):
     # not differentiable
     myfilter=tf.ones([16,16,1,1])
     y_true_reduced=tf.nn.conv2d(input=y_true, filter=myfilter, padding="VALID", strides=[1,16,16,1])
-    y_true_reduced = np.where(y_true_reduced > 3, 1, 0)
+    y_true_reduced = tf.where(y_true_reduced > 3, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
     y_pred_reduced=tf.nn.conv2d(input=y_pred, filter=myfilter, padding="VALID", strides=[1,16,16,1])
-    y_pred_reduced = np.where(y_pred_reduced > 3, 1, 0)
+    y_pred_reduced = tf.where(y_pred_reduced > 3, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
     return tf.losses.mean_squared_error(y_true_reduced,y_pred_reduced)
 
