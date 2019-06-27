3714835b2087e38ab74aa6232a84686898dae663
blappm <martin.blapp@gmail.com>
2019-06-27 16:23:12 +0200

    fixed bug when using stacked custom losses

--- seggy_unet/custom_losses.py
+++ seggy_unet/custom_losses.py
@@ -5,7 +5,7 @@ from keras.optimizers import *
 
 def grid_loss(y_true, y_pred):
     # differentiable
-    myfilter=tf.ones([16,16,1,1])
+    myfilter=tf.ones([16,16,tf.shape(y_true)[3],1])
     y_true_reduced=tf.nn.conv2d(input=y_true, filter=myfilter, padding="VALID", strides=[1,16,16,1])
     y_pred_reduced=tf.nn.conv2d(input=y_pred, filter=myfilter, padding="VALID", strides=[1,16,16,1])
     return tf.losses.mean_squared_error(y_true_reduced,y_pred_reduced)
@@ -13,7 +13,7 @@ def grid_loss(y_true, y_pred):
 
 def kaggle_metric(y_true, y_pred):
     # not differentiable
-    myfilter=tf.ones([16,16,1,1])
+    myfilter=tf.ones([16,16,tf.shape(y_true)[3],1])
     y_true_reduced=tf.nn.conv2d(input=y_true, filter=myfilter, padding="VALID", strides=[1,16,16,1])
     y_true_reduced = tf.where(y_true_reduced > 3, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
     y_pred_reduced=tf.nn.conv2d(input=y_pred, filter=myfilter, padding="VALID", strides=[1,16,16,1])
