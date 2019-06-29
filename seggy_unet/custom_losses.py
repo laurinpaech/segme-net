fee438d450b0ec9399d5fa9f68059314dbddac24
blappm <martin.blapp@gmail.com>
2019-06-29 21:19:21 +0200

    fixed stupid bug in all kaggle type metrics

--- seggy_unet/custom_losses.py
+++ seggy_unet/custom_losses.py
@@ -15,9 +15,9 @@ def kaggle_metric(y_true, y_pred):
     # not differentiable
     myfilter=tf.ones([16,16,tf.shape(y_true)[3],1])
     y_true_reduced=tf.nn.conv2d(input=y_true, filter=myfilter, padding="VALID", strides=[1,16,16,1])
-    y_true_reduced = tf.where(y_true_reduced > 3, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
+    y_true_reduced = tf.where(y_true_reduced > 63, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
     y_pred_reduced=tf.nn.conv2d(input=y_pred, filter=myfilter, padding="VALID", strides=[1,16,16,1])
-    y_pred_reduced = tf.where(y_pred_reduced > 3, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
+    y_pred_reduced = tf.where(y_pred_reduced > 63, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
 
     # only use results from last unet
     return tf.losses.mean_squared_error(y_true_reduced[:,:,:,tf.shape(y_true_reduced)[3]-1],y_pred_reduced[:,:,:,tf.shape(y_true_reduced)[3]-1])
@@ -31,9 +31,9 @@ def real_kaggle_metric(y_true, y_pred, alpha=0.5):
     y_pred_filtered = tf.where(y_pred > alpha, tf.ones(tf.shape(y_pred)), tf.zeros(tf.shape(y_pred)))
 
     y_pred_reduced=tf.nn.conv2d(input=y_pred_filtered, filter=myfilter, padding="VALID", strides=[16])
-    y_pred_reduced = tf.where(y_pred_reduced > 3, tf.ones(tf.shape(y_pred_reduced)), tf.zeros(tf.shape(y_pred_reduced)))
+    y_pred_reduced = tf.where(y_pred_reduced > 63, tf.ones(tf.shape(y_pred_reduced)), tf.zeros(tf.shape(y_pred_reduced)))
     y_true_reduced=tf.nn.conv2d(input=y_true, filter=myfilter, padding="VALID", strides=[16])
-    y_true_reduced = tf.where(y_true_reduced > 3, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
+    y_true_reduced = tf.where(y_true_reduced > 63, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
 
     # only use results from last unet
     return tf.losses.mean_squared_error(y_true_reduced,y_pred_reduced)
@@ -44,10 +44,24 @@ def real_kaggle_metric_035(y_true, y_pred):
     y_pred_filtered = tf.where(y_pred > 0.35, tf.ones(tf.shape(y_pred)), tf.zeros(tf.shape(y_pred)))
 
     y_pred_reduced=tf.nn.conv2d(input=y_pred_filtered, filter=myfilter, padding="VALID", strides=[1,16,16,1])
-    y_pred_reduced = tf.where(y_pred_reduced > 3, tf.ones(tf.shape(y_pred_reduced)), tf.zeros(tf.shape(y_pred_reduced)))
+    y_pred_reduced = tf.where(y_pred_reduced > 63, tf.ones(tf.shape(y_pred_reduced)), tf.zeros(tf.shape(y_pred_reduced)))
     y_true_reduced=tf.nn.conv2d(input=y_true, filter=myfilter, padding="VALID", strides=[1,16,16,1])
-    y_true_reduced = tf.where(y_true_reduced > 3, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
+    y_true_reduced = tf.where(y_true_reduced > 63, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
 
     # only use results from last unet
     return tf.losses.mean_squared_error(y_true_reduced,y_pred_reduced)
 
+
+
+def prob_kaggle_metric(y_true, y_pred, beta=3):
+    # not differentiable
+    myfilter=tf.ones([16,16,tf.shape(y_true)[3],1])
+    y_true_reduced=tf.nn.conv2d(input=y_true, filter=myfilter, padding="VALID", strides=[1,16,16,1])
+    print(np.max(y_true_reduced).eval())
+    y_true_reduced = tf.where(y_true_reduced > 63, tf.ones(tf.shape(y_true_reduced)), tf.zeros(tf.shape(y_true_reduced)))
+    y_pred_reduced=tf.nn.conv2d(input=y_pred, filter=myfilter, padding="VALID", strides=[1,16,16,1])
+    print(np.max(y_pred_reduced).eval())
+    y_pred_reduced = tf.where(y_pred_reduced > beta, tf.ones(tf.shape(y_pred_reduced)), tf.zeros(tf.shape(y_pred_reduced)))
+
+    # only use results from last unet
+    return tf.losses.mean_squared_error(y_true_reduced,y_pred_reduced)
